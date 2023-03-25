b00717581ce7afb9b4ee45470e9094e092effd43
MyNameIsClown <96545817+MyNameIsClown@users.noreply.github.com>
2023-03-25 20:09:27 +0100

    Initial commit

---
+++ src/main/java/project/ProjectGreenBackApplication.java
@@ -0,0 +1,13 @@
+package project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectGreenBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectGreenBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/ProjectGreenBackApplicationTests.java
@@ -0,0 +1,13 @@
+package project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectGreenBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
